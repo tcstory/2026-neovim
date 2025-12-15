f5ae5fa05a13e2f4befc531574e98d1037a7ba7c
tcstory <tcstory@163.com>
2025-12-15 09:49:42 +0800

    wip

---
+++ lua/config/lazy.lua
@@ -0,0 +1,48 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.number = true
+vim.opt.list = true
+vim.opt.listchars = 'extends:❯,precedes:❮,tab:▸ ,trail:˽'
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 1000
+vim.opt.clipboard = vim.opt.clipboard + 'unnamedplus'
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  branch = 'master', 
+  lazy = false, 
+  build = ":TSUpdate",
+  opts = {
+    -- LazyVim config for treesitter
+    indent = { enable = true }, ---@type lazyvim.TSFeat
+    highlight = { enable = true }, ---@type lazyvim.TSFeat
+    folds = { enable = true }, ---@type lazyvim.TSFeat
+    ensure_installed = {
+      "norg",
+      "bash",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+    },
+  },
+}
