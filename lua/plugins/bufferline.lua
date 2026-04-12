c1c7a0a44e594574f527384a5058459aa3ad30c7
tcstory <tcstory@163.com>
2026-04-12 11:35:07 +0800

    feat: configure bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,12 @@
 return {
-  'akinsho/bufferline.nvim', 
-  version = "*", 
+  'akinsho/bufferline.nvim',
+  version = "*",
+  event = "VeryLazy",
   dependencies = 'nvim-tree/nvim-web-devicons',
+  keys = {
+    { "<leader>bn", "<cmd>BufferLineCycleNext<cr>", desc = "buffer line cycle next" },
+    { "<leader>bp", "<cmd>BufferLineCyclePrev<cr>", desc = "buffer line cycle prev" }, 
+  },
   config = function()
     require('bufferline').setup({
       options = {
