2eb5429e45390909aabc56d3c84e0a28eae63747
tcstory <tcstory@163.com>
2026-04-12 14:11:15 +0800

    feat: add neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,31 @@
+return {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+
+    -- Only one of these is needed.
+    "esmuellert/codediff.nvim",      -- optional
+
+    -- For a custom log pager
+    "m00qek/baleia.nvim",            -- optional
+
+    -- Only one of these is needed.
+    "folke/snacks.nvim",             -- optional
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+  },
+  opts = {
+    graph_style = "unicode",
+    integrations = {
+      codediff = true,
+      snacks = true,
+    },
+    diff_viewer = "codediff",
+  },
+  config = function(_, opts)
+    require("neogit").setup(opts)
+  end,
+}
