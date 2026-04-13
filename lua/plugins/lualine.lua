7a5ab791f40b37394bf1850cd81bcf06bfa5c310
tcstory <tcstory@163.com>
2026-04-13 10:48:52 +0800

    feat: configure lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,8 +2,34 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local function lsp_status()
+      local bufnr = vim.api.nvim_get_current_buf()
+      local clients = vim.lsp.get_clients({ bufnr = bufnr })
+      if #clients == 0 then
+        return 'no-lsp'
+      end
+
+      local names = {}
+      for _, client in ipairs(clients) do
+        names[#names + 1] = client.name
+      end
+      return 'lsp:' .. table.concat(names, ',')
+    end
+
     require('lualine').setup({
-      options = { theme = "tokyonight" }
+      options = {
+        theme = "tokyonight",
+        component_separators = { left = '|', right = '|' },
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_x = {
+          lsp_status,
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
+      },
     })
   end,
 }
