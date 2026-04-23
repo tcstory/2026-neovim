6db6916e157ceb23e62d8d3903a18752a2aa241b
tcstory <tcstory@163.com>
2026-04-23 09:17:24 +0800

    feat: display lsp info in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,27 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup({
-      options = { theme = "tokyonight" }
+      options = { theme = "tokyonight" },
+      sections = {
+        lualine_a = {
+          {
+            'lsp_status',
+            icon = '', -- f013
+            symbols = {
+              -- Standard unicode symbols to cycle through for LSP progress:
+              spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+              -- Standard unicode symbol for when LSP is done:
+              done = '✓',
+              -- Delimiter inserted between LSP names:
+              separator = ' ',
+            },
+            -- List of LSP names to ignore (e.g., `null-ls`):
+            ignore_lsp = {},
+            -- Display the LSP name
+            show_name = true,
+          }
+        }
+      }
     })
   end,
 }
